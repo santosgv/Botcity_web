c52fde091d5494b8b3cef38f2ef4c0a5f0e99023
Vitor dos Santos Gomes <54445515+santosgv@users.noreply.github.com>
2025-03-25 20:03:25 -0300

    esdados de sp mg

--- BotWeb/bot.py
+++ BotWeb/bot.py
@@ -1,20 +1,17 @@
 
-
-# Import for the Web Bot
 from botcity.web import WebBot, Browser, By
 import time
-import tempfile
 import requests
-
 from botcity.web import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-import os
-# Import for integration with BotCity Maestro SDK
+from botcity.web.util import element_as_select
+from botcity.web.parsers import table_to_dict
+
+
 from botcity.maestro import *
 
-# Disable errors if we are not connected to Maestro
 BotMaestroSDK.RAISE_NOT_CONNECTED = False
 
 import whisper
@@ -50,7 +47,6 @@ def transcribe(audio_content):
     model = whisper.load_model("base")
     resposta = model.transcribe(audio_content)
     return resposta['text']
-
 
 def solve_audio_captcha(driver):
     """Resolve completamente o desafio de áudio do reCAPTCHA"""
@@ -124,19 +120,8 @@ def request_audio_version(driver):
         print(f"Erro: {str(e)}")
         return False
 
-
-def main():
-    # Runner passes the server url, the id of the task being executed,
-    # the access token and the parameters that this task receives (when applicable).
-    maestro = BotMaestroSDK.from_sys_args()
-    ## Fetch the BotExecution with details from the task, including parameters
-    execution = maestro.get_execution()
-
-    print(f"Task ID is: {execution.task_id}")
-    print(f"Task Parameters are: {execution.parameters}")
-
+def estado_es(CNPJ):
     bot = WebBot()
-
     # Configure whether or not to run on headless mode
     bot.headless = False
 
@@ -144,38 +129,16 @@ def main():
     bot.browser = Browser.CHROME
 
     # Uncomment to set the WebDriver path
-    #bot.driver_path =r"C:\GitHub\Botcity_web\chromedriver-win64\chromedriver.exe"
-    bot.driver_path ="/home/vitor/Documents/Projetos/Botcity_web/chromedriver-linux64/chromedriver"
+    bot.driver_path =r"C:\GitHub\Botcity_web\chromedriver-win64\chromedriver.exe"
+    #bot.driver_path ="/home/vitor/Documents/Projetos/Botcity_web/chromedriver-linux64/chromedriver"
 
     bot.browse("http://www.sintegra.es.gov.br/index.php")
 
     CNPJ = bot.find_element(selector='num_cnpj', by=By.ID)
     CNPJ.click()
-    CNPJ.send_keys('33.048.204/0001-51')
+    CNPJ.send_keys(f'{CNPJ}') # 33.048.204/0001-51 exemplo
 
-
-    bot.find_element(selector=".//iframe[@title='reCAPTCHA']",by=By.XPATH).click()
-#
-    time.sleep(5)
-    if request_audio_version(bot.driver):
-        print("Desafio de áudio solicitado com sucesso!")
-#
-        if solve_audio_captcha(bot.driver):
-            print("CAPTCHA resolvido com sucesso!")
-            
-        else:
-            print("Falha ao resolver CAPTCHA de áudio")
-    else:
-        print("Falha ao solicitar desafio de áudio.")
-
-
-
-
-    
-
-
-
-   
+    time.sleep(25)
 
     bot.find_element(selector='botaoConsultar', by=By.ID).click()
 
@@ -200,6 +163,112 @@ def main():
     #     status=AutomationTaskFinishStatus.SUCCESS,
     #     message="Task Finished OK."
     # )
+
+def estado_sp(CNPJ):
+    bot = WebBot()
+    # Configure whether or not to run on headless mode
+    bot.headless = False
+
+    # Uncomment to change the default Browser to Firefox
+    bot.browser = Browser.CHROME
+
+    # Uncomment to set the WebDriver path
+    bot.driver_path =r"C:\GitHub\Botcity_web\chromedriver-win64\chromedriver.exe"
+    #bot.driver_path ="/home/vitor/Documents/Projetos/Botcity_web/chromedriver-linux64/chromedriver"
+
+    bot.browse("https://www.cadesp.fazenda.sp.gov.br/(S(d011sknoerhidcw141efdci0))/Pages/Cadastro/Consultas/ConsultaPublica/ConsultaPublica.aspx")
+
+    label = bot.find_element(selector='ctl00_conteudoPaginaPlaceHolder_filtroTabContainer_filtroEmitirCertidaoTabPanel_tipoFiltroDropDownList', by=By.ID)
+    
+    label = element_as_select(label)
+    label.select_by_value(value='1')
+
+    time.sleep(3)
+
+    input = bot.find_element(selector='//*[@id="ctl00_conteudoPaginaPlaceHolder_filtroTabContainer_filtroEmitirCertidaoTabPanel_valorFiltroTextBox"]',by=By.XPATH)
+    input.click()
+    input.send_keys(f'{CNPJ}')
+    
+    time.sleep(25)
+
+    bot.find_element(selector='ctl00$conteudoPaginaPlaceHolder$filtroTabContainer$filtroEmitirCertidaoTabPanel$consultaPublicaButton', by=By.NAME).click()
+
+
+    DADOS = bot.find_elements(selector='dadoDetalhe',by=By.CLASS_NAME)
+
+    for ind,x in enumerate(DADOS):
+        print(f'Index: {ind} | Texto: {x.text}')
+
+
+    # Wait 3 seconds before closing
+    bot.wait(3000)
+
+    # Finish and clean up the Web Browser
+    # You MUST invoke the stop_browser to avoid
+    # leaving instances of the webdriver open
+    #bot.stop_browser()
+
+    # Uncomment to mark this task as finished on BotMaestro
+    # maestro.finish_task(
+    #     task_id=execution.task_id,
+    #     status=AutomationTaskFinishStatus.SUCCESS,
+    #     message="Task Finished OK."
+    # )
+
+def estado_mg(CNPJ):
+    bot = WebBot()
+    # Configure whether or not to run on headless mode
+    bot.headless = False
+
+    # Uncomment to change the default Browser to Firefox
+    bot.browser = Browser.CHROME
+
+    # Uncomment to set the WebDriver path
+    bot.driver_path =r"C:\GitHub\Botcity_web\chromedriver-win64\chromedriver.exe"
+    #bot.driver_path ="/home/vitor/Documents/Projetos/Botcity_web/chromedriver-linux64/chromedriver"
+
+    bot.browse("https://dfe-portal.svrs.rs.gov.br/NFE/CCC")
+
+    label=bot.find_element(selector='CodUf',by=By.ID)
+    label = element_as_select(label)
+    label.select_by_value(value='31')
+    time.sleep(3)
+
+    form = bot.find_element(selector='CodInscrMf',by=By.ID)
+    form.click()
+    form.send_keys(CNPJ)
+
+    time.sleep(20)
+    bot.find_element(selector='BtnPesquisarCodInscrMf',by=By.ID).click()
+  
+    
+
+    elemento_tabela = bot.find_element("ListaContribuintes", By.ID)
+    dados_tabela = table_to_dict(table=elemento_tabela)
+
+    print(dados_tabela)
+
+    for linha in dados_tabela:
+        print(f'''{linha}
+    ''')
+
+    
+
+
+def main():
+    # Runner passes the server url, the id of the task being executed,
+    # the access token and the parameters that this task receives (when applicable).
+    maestro = BotMaestroSDK.from_sys_args()
+    ## Fetch the BotExecution with details from the task, including parameters
+    execution = maestro.get_execution()
+
+    print(f"Task ID is: {execution.task_id}")
+    print(f"Task Parameters are: {execution.parameters}")
+
+    #estado_es('33.048.204/0001-51') # 33.048.204/0001-51 aceita os caracteres
+    #estado_sp('05328923000190') # 05.328.923/0001-90 exemplo nao aceita com . /-
+    #estado_mg('42.274.696/0096-55') # mg 42.274.696/0096-55
+
 
 
 def not_found(label):
